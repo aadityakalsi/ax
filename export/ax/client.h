53f5e7c02149f7c960ca2b8d1c2b3123f760c30e
Aaditya Kalsi <akalsi@mathworks.com>
2017-08-16 14:32:29 -0400

    Add basic client and automated server test

---
+++ export/ax/client.h
@@ -0,0 +1,24 @@
+/*
+For license details see ../../LICENSE
+*/
+
+/**
+ * \file client.h
+ * \date Aug 16, 2017
+ */
+
+#ifndef _AX_CLIENT_H_
+#define _AX_CLIENT_H_
+
+AX_HIDDEN_TYPE(ax_client_t, 1536);
+
+AX_API
+int ax_client_init_ip4(ax_client_t* srv, ax_const_str addr, int port);
+
+AX_API
+int ax_client_destroy(ax_client_t* srv);
+
+AX_API
+int ax_client_run_once(ax_client_t* srv);
+
+#endif/*_AX_CLIENT_H_*/
--- tests/server.c
+++ tests/server.c
@@ -10,6 +10,8 @@ For license details see ../../LICENSE
 #include "defs.h"
 
 #include "ax/server.h"
+#include "ax/client.h"
+#include "ax/thread.h"
 
 void srvr_create_destroy(void)
 {
@@ -18,14 +20,43 @@ void srvr_create_destroy(void)
     testThat(ax_server_destroy(&srv) == 0);
 }
 
+/*--------------------------------------------------*/
+
+typedef struct
+{
+    ax_client_t client;
+    ax_u32 conn;
+    ax_u32 conn_ack;
+} wait_client_t;
+
+static
+void wait_and_connect(void* a)
+{
+    wait_client_t* c = (wait_client_t*)a;
+    while (c->conn != 1) { }
+    c->conn_ack = 1;
+    ax_client_run_once(&c->client);
+}
+
 void srvr_create_connect_destroy(void)
 {
     ax_server_t srv;
+    wait_client_t cli;
+    ax_thread_t cli_tid;
+    cli.conn = 0;
+    cli.conn_ack = 0;
     testThat(ax_server_init_ip4(&srv, "localhost", 8080) == 0);
+    testThat(ax_client_init_ip4(&cli.client, "localhost", 8080) == 0);
+    testThat(ax_thread_create(&cli_tid, wait_and_connect, &cli) == 0);
+    cli.conn = 1;
+    while (cli.conn_ack == 0) { }
     testThat(ax_server_run_once(&srv) == 1);
     testThat(ax_server_destroy(&srv) == 0);
+    testThat(ax_thread_join(&cli_tid) == 0);
+    testThat(ax_client_destroy(&cli.client) == 0);
 }
 
+/*--------------------------------------------------*/
 setupSuite(server)
 {
     addTest(srvr_create_destroy);
