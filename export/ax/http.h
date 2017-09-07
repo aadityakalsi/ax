2ffd5b87696681463e7b813c340c92c3925335ed
Aaditya Kalsi <akalsi.dev@gmail.com>
2017-09-06 21:54:41 -0400

    Add http skeleton

---
+++ export/ax/http.h
@@ -0,0 +1,13 @@
+/*
+For license details see ../../LICENSE
+*/
+
+/**
+ * \file http.h
+ * \date Sep 06, 2017
+ */
+
+#ifndef _AX_HTTP_H_
+#define _AX_HTTP_H_
+
+#endif/*_AX_HTTP_H_*/
---
+++ src/ax/http.c
@@ -0,0 +1,9 @@
+/*
+For license details see ../../LICENSE
+*/
+
+/**
+ * \file http.c
+ * \date Sep 06, 2017
+ */
+
